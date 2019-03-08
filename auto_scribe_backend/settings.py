409bcb54f74e9953bc2cac3f31e1407bc6c58c46
jessamynsmith <jessamyn.smith@gmail.com>
2019-03-07 23:48:44 -0700

    Added heroku setup

--- auto_scribe_backend/settings.py
+++ auto_scribe_backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -120,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+django_heroku.settings(locals())
