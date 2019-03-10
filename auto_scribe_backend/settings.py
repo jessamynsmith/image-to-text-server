71663be14fd06c831535a580a2b72a5f326be269
jessamynsmith <jessamyn.smith@gmail.com>
2019-03-10 14:57:57 -0600

    Added favicon.

--- auto_scribe_backend/settings.py
+++ auto_scribe_backend/settings.py
@@ -129,6 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'auto_scribe_backend', 'static'),
+]
 
 
 ADMINS = (
