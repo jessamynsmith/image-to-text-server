05e11df28b9c4c0da9127078e230f8eaddcd0938
jessamynsmith <geekchick77@gmail.com>
2023-02-03 21:23:30 -0700

    Renamed project from image_to_text_server to image2text

--- image2text/wsgi.py
+++ image2text/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for image_to_text_server project.
+WSGI config for image2text project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'image_to_text_server.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'image2text.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'image_to_text_server.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'image2text.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
